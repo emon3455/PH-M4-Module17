0cb84cc7e60ade4dcb503a9a5988acf24907cf04
emon3455 <emon.mhk69@gmail.com>
2023-01-25 22:54:00 +0600

    basic mathmetical operations

---
+++ MathemeticalOperation.js
@@ -0,0 +1,27 @@
+// x + y = 10;
+/*
+here x and y is operator 
+and + is operand.
+*/
+
+// addition operation
+var apple = 10;
+var mango = 15;
+var totall = apple + mango;
+console.log(totall);
+
+
+// substraction operation:
+var difference = mango - apple;
+console.log(difference);
+
+// multiplication
+var quantity = 7;
+var price = apple * quantity;
+console.log(price);
+
+// division 
+var totallFuitCost = 56;
+var totallFruitsAmount = 7;
+var eachFruitsPrice = totallFuitCost / totallFruitsAmount;
+console.log(eachFruitsPrice);
