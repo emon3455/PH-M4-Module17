9d560ecb4180ffb58a7e80898101bf1ebc4655c7
emon3455 <emon.mhk69@gmail.com>
2023-01-25 21:58:11 +0600

    variable with 3 data types

--- variable.js
+++ variable.js
@@ -5,3 +5,14 @@ var age = 20;
 var profesion = "Student";
 var weight = 87 ;
 console.log ("My name is "+name+ " i am a "+ profesion + "i am "+weight+ " Kg");
+
+
+// numeric data type:
+var marks= 22;
+
+// string data type:
+var message = "i am a programmer";
+
+// boolean data type:
+var single = false;
+var singer = true;
