66532b9e1d366c55056ba0c2c78380c5f798123c
emon3455 <emon.mhk69@gmail.com>
2023-01-25 22:44:43 +0600

    keyword and naming or variable

---
+++ keyValue_Naming.js
@@ -0,0 +1,11 @@
+// we cannot use key value as variable name
+
+/*
+
+*** variable name cannot start with number
+*** we can only use under score in variable we cannot any special word
+*** in js we will use camel case naming convention
+
+*/
+
+var userName = "emon"; //camel case
