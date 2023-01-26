7bbc6110937872b3aeebdf3715cc44dda1cc8b80
emon3455 <emon.mhk69@gmail.com>
2023-01-26 13:46:13 +0600

    practice problem-1 solved

---
+++ problem1.js
@@ -0,0 +1,7 @@
+// Harry’s mom gave him money and asked him to buy some oranges and apples. Write a program to
+// help Harry calculate how much money the shopkeeper will return.
+
+var totalMoney = 1000;
+var fruitsPrice = 700;
+var remainMoney = totalMoney - fruitsPrice;
+console.log(remainMoney);
